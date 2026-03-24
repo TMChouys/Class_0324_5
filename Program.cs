a728e00848d4e7d6ae5146e6acc17886f169f071
TMChouys <chouys@gs.takming.edu.tw>
2026-03-24 16:15:02 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Class_0324_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Student.Count);
+
+
+            Student joe;
+            joe = new Student(1234, "AAA", 92, 88);
+            Console.WriteLine(joe.GetStudent());
+            Console.WriteLine(Student.Count);
+
+            Student mary = new Student(1234, "AAA", 92, 88);
+            Console.WriteLine(Student.Count);
+
+            Student tom = new Student(1234, "AAA", 92, 88);
+            Console.WriteLine(Student.Count);
+
+        }
+    }
+}
